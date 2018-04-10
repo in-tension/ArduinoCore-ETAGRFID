15db760ff215763a012a9643661c44c90cb69f3c
maxwell Mullin <mullinmax@gmail.com>
2018-04-10 18:36:55 -0400

    maybe correct json

--- release.py
+++ release.py
@@ -31,8 +31,12 @@ print(fileSize)
 JSONstring = '{"packages":[{"name":"arduino-beta","maintainer":"ArduinoBetatesting","websiteURL":"http://www.arduino.cc/","email":"packages@arduino.cc","help":{"online":"http://www.arduino.cc/en/Reference/HomePage"},"platforms":[{"archiveFileName":"package_ETAGRFID_index.tar.bz2","name":"ArduinoETAGRFIDcore","architecture":"samd","category":"Arduino",'
 versionCode = '1.0.0'
 JSONstring += '"version": "' + versionCode + '",'
-JSONstring += '"url": "http://downloads.arduino.cc/Hourly/samd/package_samd-hourly-b184.tar.bz2",'
+JSONstring += '"url": "https://github.com/jaywilhelm/ArduinoCore-ETAGRFID/raw/master/ArduinoCoreETAGRFID.tar.bz2",'
 JSONstring += '"checksum": "SHA-256:' + checksum + '",'
 JSONstring += '"size": "' + str(fileSize) + '",'
 JSONstring += '"boards":[{"name":"ArduinoZero"}],"toolsDependencies":[{"packager":"arduino","name":"arm-none-eabi-gcc","version":"4.8.3-2014q1"},{"packager":"arduino","name":"bossac","version":"1.7.0"},{"packager":"arduino","name":"openocd","version":"0.9.0-arduino6-static"},{"packager":"arduino","name":"CMSIS","version":"4.5.0"},{"packager":"arduino","name":"CMSIS-Atmel","version":"1.1.0"},{"packager":"arduino","name":"arduinoOTA","version":"1.2.0"}]}],"tools":[]}]}'
 print(JSONstring)
+
+f= open("package_ETAGRFID_index.json","w+")
+f.write(JSONstring)
+f.close()
